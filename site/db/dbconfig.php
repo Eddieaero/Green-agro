2bf1bd2f789385f870ad68153d5023ba3d5cf100
Eddieaero <firstedson@gmail.com>
2023-04-28 12:54:53 +0300

    adjustments

--- site/db/dbconfig.php
+++ site/db/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "localhost";
+$password = "edson000";
 $dbname = "greenagro";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
--- site/soil_management.php
+++ site/soil_management.php
@@ -205,7 +205,7 @@ function soilManagementSuggestions($plantName, $soilMoisture, $soilTemperature)
             <div class="d-flex align-items-center"><a class="menu-btn d-flex align-items-center justify-content-center p-2 bg-gray-900" id="toggle-btn" href="#">
                 <svg class="svg-icon svg-icon-sm svg-icon-heavy text-white">
                   <use xlink:href="#menu-1"> </use>
-                </svg></a><a class="navbar-brand ms-2" href="index.html">
+                </svg></a><a class="navbar-brand ms-2" href="index.php">
                 <div class="brand-text d-none d-md-inline-block text-uppercase letter-spacing-0"><span class="text-white fw-normal text-xs"> </span><strong class="text-primary text-sm">Field Management</strong></div>
               </a></div>
             <ul class="nav-menu mb-0 list-unstyled d-flex flex-md-row align-items-md-center">
